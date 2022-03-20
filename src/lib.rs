38aff3ecf6d67620a4cf16b9fde367de5d44cc49
freeze-dolphin <freeze-dolphin@outlook.com>
2022-03-20 13:23:13 +0800

    a try of multi-process

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
-use std::{fs, process};
+use std::hash::Hash;
+use std::sync::mpsc;
+use std::thread::JoinHandle;
+use std::{fs, process, thread};
+
 use clap::Parser;
 use regex::Regex;
 
@@ -14,7 +18,6 @@ pub struct RuntimeArgs {
     #[clap(short, long, default_value_t = 1)]
     count: u8,
      */
-
     #[clap(short, long)]
     pub filter: String,
 
@@ -22,12 +25,16 @@ pub struct RuntimeArgs {
     pub input: String,
 
     #[clap(short, long)]
-    pub case_sensitive: bool,
+    pub case_insensitive: bool,
 
     #[clap(short, long)]
     pub raw_print: bool,
 
+    #[clap(short, long, default_value_t = 100000)]
+    pub limit: usize,
 }
+
+type StatisticBuffer = HashMap<String, u32>;
 
 pub fn read(args: &RuntimeArgs) -> String {
     fs::read_to_string(&args.input).unwrap_or_else(|err| {
@@ -36,44 +43,97 @@ pub fn read(args: &RuntimeArgs) -> String {
     })
 }
 
-pub fn statistics(content: &String, case_sensitive: &bool) -> HashMap<String, u32> {
-    let mut db: HashMap<String, u32> = HashMap::new();
-    content.split_whitespace().for_each(|f| {
-        let f = Regex::new("[a-zA-Z]*").expect("unknown error").captures(f).unwrap().get(0).unwrap().as_str();
-
-        if *case_sensitive {
-            db.insert(String::from(f), db.get(f).unwrap_or_else(|| &0) + 1);
-        } else {
-            let f = &f.to_lowercase();
-            db.insert(String::from(f), db.get(f).unwrap_or_else(|| &0) + 1);
-        }
-    });
-    db
+fn merge_noref<K: Eq + Hash, V>(map1: HashMap<K, V>, map2: HashMap<K, V>) -> HashMap<K, V> {
+    map1.into_iter().chain(map2).collect()
 }
 
-pub fn filter(db: &HashMap<String, u32>, exp: Regex) -> HashMap<String, u32> {
-    let mut dbc = db.clone();
-    dbc.retain(|f, _| {
-        exp.is_match(&f)
+pub fn statistics(content: &String, case_insensitive: bool, limit: usize) -> StatisticBuffer {
+    let mut hdls: Vec<JoinHandle<()>> = Vec::new();
+    let mut splited: Vec<String> = Vec::new();
+    content.split_whitespace().for_each(|f| {
+        let mut f = f.to_string();
+
+        if !case_insensitive {
+            f = f.to_lowercase();
+        }
+        splited.push(f.to_string());
     });
+
+    let (tsm, rcv) = mpsc::channel();
+
+    let mut times = splited.len() / limit;
+    if times == 0 {
+        times = 1;
+    }
+
+    for n in 0..times {
+        let sliced = splited[n * limit..(n + 1) * limit].to_vec();
+
+        hdls.push(thread::spawn(move || {
+            let mut tmp = StatisticBuffer::new();
+            sliced.iter().for_each(|f| {
+                let cpt = Regex::new("[a-zA-Z]*")
+                    .unwrap()
+                    .captures(f)
+                    .unwrap()
+                    .get(0)
+                    .unwrap()
+                    .as_str();
+                tmp.insert(String::from(cpt), tmp.get(cpt).unwrap_or_else(|| &0) + 1);
+            });
+            tsm.send(tmp).unwrap();
+        }));
+    }
+
+    let mut rst = StatisticBuffer::new();
+
+    for hdl in hdls {
+        hdl.join().unwrap();
+        rst = merge_noref(rst, rcv.recv().unwrap());
+    }
+
+    rst
+}
+
+pub fn filter(db: &StatisticBuffer, exp: Regex) -> StatisticBuffer {
+    let mut dbc = db.clone();
+    dbc.retain(|f, _| exp.is_match(&f));
     dbc
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
+
     use regex::Regex;
-    use crate::filter;
+
+    use crate::{filter, StatisticBuffer};
 
     #[test]
     fn test_filter() {
-        let mut m1: HashMap<String, u32> = HashMap::new();
+        let mut m1: StatisticBuffer = HashMap::new();
         m1.insert("test".to_string(), 3);
         m1.insert("hello".to_string(), 2);
 
-        let mut m2: HashMap<String, u32> = m1.clone();
+        let mut m2: StatisticBuffer = m1.clone();
         m2.remove("hello");
 
-        assert_eq!(filter(&m1, Regex::new(".*es.*").expect("unable to create regex")), m2);
+        assert_eq!(
+            filter(&m1, Regex::new(".*es.*").expect("unable to create regex")),
+            m2
+        );
     }
 }
+
+/*
+
+let b = true;
+let mut f = "AWA";
+
+if b {
+    f = &f.to_lowercase();
+}
+
+println!("{}", f);
+
+ */
