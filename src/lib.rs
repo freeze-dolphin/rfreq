ff9b21a83eb7b8f48c503e8dd424f1d80c88fbe9
freeze-dolphin <freeze-dolphin@outlook.com>
2022-03-26 15:25:57 +0800

    feat. sorting

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,14 @@ pub struct RuntimeArgs {
     #[clap(short, long, help = "File to perform analysis on")]
     pub input: String,
 
-    #[clap(short, long, help = "Turn on case insensitive")]
-    pub case_insensitive: bool,
+    #[clap(short, long, help = "Consider different cases as different words")]
+    pub case_sensitive: bool,
 
     #[clap(short, long, help = "Print result without using table form")]
     pub raw_print: bool,
+
+    #[clap(short, long, help = "Disable sorting")]
+    pub no_sorting: bool,
 }
 
 type StatisticBuffer = HashMap<String, u32>;
@@ -28,7 +31,7 @@ pub fn read(args: &RuntimeArgs) -> String {
     })
 }
 
-pub fn statistics(content: &str, case_insensitive: bool) -> StatisticBuffer {
+pub fn statistics(content: &str, case_sensitive: bool) -> StatisticBuffer {
     let mut db: StatisticBuffer = StatisticBuffer::new();
     content.split_whitespace().for_each(|f| {
         let s = Regex::new("[a-zA-Z]*")
@@ -41,12 +44,19 @@ pub fn statistics(content: &str, case_insensitive: bool) -> StatisticBuffer {
 
         let mut f = s.to_string();
 
-        if case_insensitive {
+        if !case_sensitive {
             f = f.to_lowercase();
         }
         db.insert(f, db.get(s).unwrap_or(&0) + 1);
     });
     db
+}
+
+pub fn sort(db: &StatisticBuffer) -> Vec<(&String, &u32)> {
+    let mut sort: Vec<_> = db.iter().collect();
+    sort.sort_by(|a, b| b.1.cmp(a.1));
+
+    sort
 }
 
 pub fn filter(db: &StatisticBuffer, exp: Regex) -> StatisticBuffer {
@@ -57,21 +67,34 @@ pub fn filter(db: &StatisticBuffer, exp: Regex) -> StatisticBuffer {
 
 #[cfg(test)]
 mod tests {
-    use crate::{filter, StatisticBuffer};
+    use crate::{filter, sort, StatisticBuffer};
     use regex::Regex;
 
     #[test]
     fn test_filter() {
-        let mut m1: StatisticBuffer = StatisticBuffer::new();
+        let mut m1 = StatisticBuffer::new();
         m1.insert("test".to_string(), 3);
         m1.insert("hello".to_string(), 2);
 
-        let mut m2: StatisticBuffer = m1.clone();
+        let mut m2 = m1.clone();
         m2.remove("hello");
 
         assert_eq!(
             filter(&m1, Regex::new(".*es.*").expect("unable to create regex")),
             m2
+        );
+    }
+
+    #[test]
+    fn test_sort() {
+        let mut m = StatisticBuffer::new();
+        m.insert("test".to_string(), 3);
+        m.insert("hello".to_string(), 1);
+        m.insert("good".to_string(), 10);
+        let s = sort(&m);
+        assert_eq!(
+            format!("{:?}", s),
+            "[(\"good\", 10), (\"test\", 3), (\"hello\", 1)]"
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+#[macro_use]
+extern crate prettytable;
+
+use std::time::{Duration, Instant};
+
 use clap::Parser;
 use prettytable::{format, row, Table};
 use regex::Regex;
-use rfreq::RuntimeArgs;
-use std::time::Instant;
 
-#[macro_use]
-extern crate prettytable;
+use rfreq::RuntimeArgs;
 
 fn main() {
     let args: RuntimeArgs = RuntimeArgs::parse();
@@ -17,18 +19,31 @@ fn main() {
     let time_cost_read = start.elapsed();
     let start = Instant::now();
 
-    let stas = rfreq::statistics(content.as_str(), args.case_insensitive);
+    let stas = rfreq::statistics(content.as_str(), args.case_sensitive);
 
     let time_cost_statstics = start.elapsed();
     let start = Instant::now();
 
     let mut exp = args.filter;
-    if !args.case_insensitive {
+    if !args.case_sensitive {
         exp = exp.to_lowercase();
     }
     let filter = rfreq::filter(&stas, Regex::new(&exp).expect("illegal regular expression"));
 
     let time_cost_analyse = start.elapsed();
+
+    let mut time_cost_sort = Duration::from_millis(0);
+
+    let result_non_sort = &filter;
+    let mut result_sort = vec![];
+    let mut sorted = false;
+
+    if !args.no_sorting {
+        let start = Instant::now();
+        result_sort = rfreq::sort(&filter);
+        time_cost_sort = start.elapsed();
+        sorted = true;
+    }
 
     if stas.is_empty() {
         println!("nothing found");
@@ -37,24 +52,37 @@ fn main() {
 
     if args.raw_print {
         println!("word\tcount");
-        filter.iter().for_each(|(word, times)| {
-            println!("{}\t{}", word, times);
-        });
+        if sorted {
+            for i in result_sort {
+                println!("{}\t{}", i.0, i.1);
+            }
+        } else {
+            result_non_sort.iter().for_each(|(word, times)| {
+                println!("{}\t{}", word, times);
+            });
+        }
     } else {
         let mut table = Table::new();
         table.set_format(*format::consts::FORMAT_CLEAN);
         table.add_row(row![b -> "word", b -> "count"]);
-        filter.iter().for_each(|(word, times)| {
-            table.add_row(row![word, times]);
-        });
+        if sorted {
+            for i in result_sort {
+                table.add_row(row![i.0, i.1]);
+            }
+        } else {
+            result_non_sort.iter().for_each(|(word, times)| {
+                table.add_row(row![word, times]);
+            });
+        }
 
         table.printstd();
     }
 
     println!(
-        "\ntime costs:\n - read: {}ms\n - statistics: {}ms\n - filter: {}ms",
+        "\ntime costs:\n - read: {}ms\n - statistics: {}ms\n - filter: {}ms\n - sort: {}ms",
         time_cost_read.as_millis(),
         time_cost_statstics.as_millis(),
-        time_cost_analyse.as_millis()
+        time_cost_analyse.as_millis(),
+        time_cost_sort.as_millis(),
     );
 }
